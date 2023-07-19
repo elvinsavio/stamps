8e22e43607119bdd14825cb8b99ec63b4fc007b8
elvinsavio <elvinsavio.lp@gmail.com>
2023-07-19 21:27:03 +0530

    add config files

---
+++ src/appConfig/landingPage.ts
@@ -0,0 +1,20 @@
+export const LandingConfig = {
+  slideDelay: 2500,
+  slideShow: [
+    {
+      imgSrc: `https://picsum.photos/id/202/2200/2200`,
+      caption: "This is some Random Caption",
+      subCaption: "This is a smaller random caption",
+    },
+    {
+      imgSrc: `https://picsum.photos/id/212/2200/2200`,
+      caption: "Hello world",
+      subCaption: "This is a smaller random caption for a smaller hello world",
+    },
+    {
+      imgSrc: `https://picsum.photos/id/282/2200/2200`,
+      caption: "This is a caption with a really really long text for it doesnt overflow ",
+      subCaption: "This is a smaller caption with a really really long text for it doesnt overflow ",
+    },
+  ],
+};
