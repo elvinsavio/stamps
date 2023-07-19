fa0d72941e7184da3aaa5263bcf77ba3d25221a9
elvinsavio <elvinsavio.lp@gmail.com>
2023-07-19 21:29:18 +0530

    add shadow for header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ import { config } from "../appConfig/appConfig";
 export default function Header() {
   const navigate = useNavigate();
   return (
-    <div className={`bg-primary font-georgia  px-2 flex justify-center`} style={{
+    <div className="bg-primary font-georgia  px-2 flex justify-center shadow-xl" style={{
       height: `${config.headerHeight}px`
     }}>
       <div className="flex justify-between items-center  w-full">
