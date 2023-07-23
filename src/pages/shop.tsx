240e58c6cb40ae1a2462fbe0c5555f03b95e9934
elvinsavio <elvinsavio.lp@gmail.com>
2023-07-23 17:34:19 +0530

    change card design

--- src/pages/shop.tsx
+++ src/pages/shop.tsx
@@ -2,14 +2,10 @@ import { shopConfig } from "../appConfig/shopConfig";
 
 export default function Shop() {
   return (
-    <div className="container my-2 mx-auto px-2 md:px-12">
-      <div className="flex flex-wrap -mx-1 lg:-mx-4">
+    <div className="">
+      <div className="flex flex-wrap ">
         {shopConfig?.shop?.products?.map((product, index) => {
-          return (
-            <div key={index} className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3 justify-center flex">
-              <ProductCard product={product} />
-            </div>
-          );
+          return <ProductCard key={index} product={product} />;
         })}
       </div>
     </div>
@@ -24,6 +20,7 @@ interface IProduct {
   price: number;
   discount: number | undefined;
   imgSrc: string;
+  redirectUrl: string;
 }
 
 interface IProductCard {
@@ -32,26 +29,34 @@ interface IProductCard {
 
 const ProductCard = ({ product }: IProductCard) => {
   return (
-    <div className="w-full font-mark rounded-lg shadow-lg snap-start flex flex-col overflow-hidden">
-      <div style={{ backgroundImage: `url(${product?.imgSrc})` }} className=" bg-cover bg-center h-[200px]" />
+    <div className="w-[260px] m-3 xsm:m-auto xsm:my-2 font-mark rounded-lg snap-start shadow flex flex-col overflow-hidden transition-all hover:-translate-y-1">
+      <div
+        style={{ backgroundImage: `url(${product?.imgSrc})` }}
+        className=" bg-cover bg-center h-[200px] transition-all"
+      />
       <div className="flex flex-col justify-between flex-1">
         <div className="p-1">
-          <p className="text-base">{product?.title}</p>
+          <p className="text-base line-clamp-2 ">{product?.title}</p>
           <p className="text-sm text-black/80">{product?.description}</p>
           {product?.by && <p className="text-sm text-black/70">By {product?.by}</p>}
         </div>
-        <div className="p-1">
-          <div className="border my-1" />
+        <div className="flex flex-col">
+          <div className="p-1">
+            <div className="my-1 border" />
 
-          {product?.discount ? (
-            <div className="flex gap-2 items-center">
-              <span className="">{product?.price - product?.price * (product?.discount / 100)}₹</span>
-              <span className="line-through text-sm">{product?.price}₹</span>
-              <span className="bg-red-200 px-2 text-sm">{product?.discount}%</span>
-            </div>
-          ) : (
-            <span className="">{product?.price}₹</span>
-          )}
+            {product?.discount ? (
+              <div className="flex items-center gap-2">
+                <span className="">{product?.price - product?.price * (product?.discount / 100)}₹</span>
+                <span className="text-sm line-through">{product?.price}₹</span>
+                <span className="px-2 text-sm bg-red-200">{product?.discount}%</span>
+              </div>
+            ) : (
+              <span className="">{product?.price}₹</span>
+            )}
+          </div>
+          <a href={product?.redirectUrl} target="_blank" className="w-full p-1 font-bold text-center bg-green-500">
+            Shop
+          </a>
         </div>
       </div>
     </div>
