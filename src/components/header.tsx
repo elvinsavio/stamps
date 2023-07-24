76a75de0bbce90d94d47a28eb6e6fff72200de5b
elvinsavio <elvinsavio.lp@gmail.com>
2023-07-24 10:16:46 +0530

    remove unused imports

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { config } from "../appConfig/appConfig";
