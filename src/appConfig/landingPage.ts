a3b322bd5c77754de410293bfd9a5599e3046576
elvinsavio <elvinsavio.lp@gmail.com>
2023-07-28 21:32:06 +0530

    un-split sections

--- src/appConfig/landingPage.ts
+++ src/appConfig/landingPage.ts
@@ -2,17 +2,17 @@ export const LandingConfig = {
   slideDelay: 2500,
   slideShow: [
     {
-      imgSrc: `https://picsum.photos/id/202/2200/2200`,
+      imgSrc: `https://picsum.photos/id/202/3200/2200`,
       caption: "This is some Random Caption",
       subCaption: "This is a smaller random caption",
     },
     {
-      imgSrc: `https://picsum.photos/id/212/2200/2200`,
+      imgSrc: `https://picsum.photos/id/212/3200/2200`,
       caption: "Hello world",
       subCaption: "This is a smaller random caption for a smaller hello world",
     },
     {
-      imgSrc: `https://picsum.photos/id/282/2200/2200`,
+      imgSrc: `https://picsum.photos/id/282/3200/2200`,
       caption: "This is a caption with a really really long text for it doesnt overflow ",
       subCaption: "This is a smaller caption with a really really long text for it doesnt overflow ",
     },
--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -12,24 +12,27 @@ import Button from "../components/ui/buttons";
 export default function LandingPage() {
   return (
     <div className="">
-      <SplitSection
+      <div
         className="snap-none"
         style={{
           height: window.innerHeight - config.headerHeight + 10,
         }}
       >
-        <div className="w-full sm:w-1/2">
+        <div className="relative w-full h-full">
+          <div className="absolute z-10 -translate-x-1/2 top-1/2 left-1/2">
+            <h1 className="text-4xl font-mark text-primary">{LandingConfig?.landingText}</h1>
+          </div>
           <SlideShow />
         </div>
-        <div className="flex-col justify-around hidden w-full px-5 py-5 bg-primary/30 sm:py-0 sm:flex">
+        {/* <div className="flex-col justify-around hidden w-full px-5 py-5 bg-primary/30 sm:py-0 sm:flex">
           <h1 className="text-xl">[logo here]</h1>
-          <h1 className="text-[3rem] w-1 font-anvir">{LandingConfig?.landingText}</h1>
+          <h1 className="text-[3rem] w-1 font-anvir"></h1>
 
           <div className="flex justify-center w-full">
             <Button>Shop Now</Button>
           </div>
-        </div>
-      </SplitSection>
+        </div> */}
+      </div>
 
       <SplitSection
         className="justify-around snap-start"
@@ -73,16 +76,15 @@ const SlideShow = () => {
         return (
           <SwiperSlide key={index}>
             <div
-              className="relative h-full bg-cover"
+              className="relative h-full bg-center bg-no-repeat bg-cover"
               style={{
                 backgroundImage: `url(${slide?.imgSrc})`,
                 height: window.innerHeight - config.headerHeight + 10,
               }}
-            >
-              <div className="absolute w-full -translate-x-1/2 sm:bottom-10 px-14 bottom-1/2 font-mark left-1/2 text-primary">
-                <h1 className="text-3xl">{slide?.caption}</h1>
-                <h2>{slide?.subCaption}</h2>
-              </div>
+            />
+            <div className="absolute w-full -translate-x-1/2 sm:bottom-10 px-14 bottom-1/2 font-mark left-1/2 text-primary">
+              <h1 className="text-3xl">{slide?.caption}</h1>
+              <h2>{slide?.subCaption}</h2>
             </div>
           </SwiperSlide>
         );
